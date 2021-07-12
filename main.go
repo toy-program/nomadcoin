8d510776db44beb6f7f0ca76fca33fa7c601e2ae
changhoi <0417kch@naver.com>
2021-07-12 12:10:55 +0900

    api

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "github.com/toy-program/nomadcoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/toy-program/nomadcoin/blockchain"
+	"github.com/toy-program/nomadcoin/utils"
+)
+
+const port string = ":3000"
+
+type URL string
+
+func (u *URL) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, *u)
+	return []byte(url), nil
+}
+
+type URLDescription struct {
+	URL         URL    `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
+}
+
+type AddBlockBody struct {
+	Data string `json:"data"`
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{URL: URL("/"), Method: "GET", Description: "See Documentations"},
+		{URL: URL("/blocks"), Method: "POST", Description: "Add a Block", Payload: "data: string"},
+	}
+
+	rw.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(rw).Encode(data)
+}
+
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Add("Content-Type", "application/json")
+	switch r.Method {
+	case "GET":
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+	case "POST":
+		var addBlockBody AddBlockBody
+		err := json.NewDecoder(r.Body).Decode(&addBlockBody)
+		utils.HandleErr(err)
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Data)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	http.HandleFunc("/blocks", blocks)
+
+	fmt.Printf("Listening on %s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
